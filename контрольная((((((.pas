de8570e947c315ff467f2433f5cdae9e9109109b
скрбн <144677300+skrbn@users.noreply.github.com>
2023-10-12 12:31:18 +0300

    Add files via upload

---
+++ контрольная((((((.pas
@@ -0,0 +1,14 @@
+﻿var x,y: real;
+begin
+  write ('x= ');
+  read(x);
+  if x<-9 then 
+    y:=tan(x)*ln(x)
+  else 
+    if (-9<=x) and (x<-3) then
+      y:=(exp(x))- power(x,1/3)
+    else
+      if -3<=x then
+        y:=cos(2*x)-cos(x);
+   writeln('y= ', y);
+end.
