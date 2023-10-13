26fa76607706577fa130dcc9d0e7107d707dc84f
скрбн <144677300+skrbn@users.noreply.github.com>
2023-10-13 18:13:21 +0300

    Add files via upload

--- контрольная((((((.pas
+++ контрольная((((((.pas
@@ -5,10 +5,9 @@ begin
   if x<-9 then 
     y:=tan(x)*ln(x)
   else 
-    if (-9<=x) and (x<-3) then
+    if(x<-3) then
       y:=(exp(x))- power(x,1/3)
     else
-      if -3<=x then
         y:=cos(2*x)-cos(x);
    writeln('y= ', y);
 end.
--- контрольная2((((((((((.pas
+++ контрольная2((((((((((.pas
@@ -6,9 +6,8 @@ while x<=-1 do
   begin
   if x<-9 then 
     writeln ('y = ',tan(x)*ln(x):1:2) else 
-    if (x>=-9) and (x<3) then
+    if (x<3) then
        writeln ('y = ', (exp(x))-power(x,1/3):1:2) else
-      if x>=-3 then 
         writeln ('y = ',cos(2*x)-cos(x):1:2); 
             x:=x+0.2;
       end;
